2aa27babd144892e96288dfd84256cc4614c8c48
SAVING-Liu <1528839363@qq.com>
2017-02-09 10:47:03 +0800

    2017年2月9日

--- js/app/activity.js
+++ js/app/activity.js
@@ -13,4 +13,24 @@ angular.module("myApp",["eunne","shezglobal","lavidione","aeannelog","fagire","m
 $(function(){
    $("footer").load("footer.html");
 });
-
+/*backtop*/
+$(function(){
+   //回到顶部
+   $(".changebg").eq(3).on("click",function(){
+      $("html,body").stop().animate({scrollTop:0},500);
+   })
+   //点击span div消失
+   $(".closemenu").on("click",function(){
+      $("#backtop").css("display","none");
+   })
+   //hover 变换背景
+   $(".changebg").hover(function(){
+      $(this).css({
+         backgroundPosition:"-68px "+(-1-58*($(this).index()-1))+"px"
+      })
+   },function(){
+      $(this).css({
+         backgroundPosition:"0px "+(-1-58*($(this).index()-1))+"px"
+      })
+   })
+})
